11164e57b659e8a44d3a03eca2b0206034133ecf
Headon02 <hannahsowlpost@gmail.com>
2021-09-07 18:08:58 +0200

    Add files via upload

--- game.py
+++ game.py
@@ -4,96 +4,212 @@ import time
 
 size = os.get_terminal_size()
 
-field = [[0 for i in range(size[0])] for i in range(size[1])]
-output = [[0 for i in range(size[0])] for i in range(size[1])]
-duplicate = [[0 for i in range(size[0])] for i in range(size[1])]
+field = [[" " for i in range(size[0])] for i in range(size[1])]
+duplicate = [[" " for i in range(size[0])] for i in range(size[1])]
 
 # patterns
 patterns = {
-    0: [[1, 1],
-        [1, 1]], # block
-    1: [[0, 1, 1, 0],
-        [1, 0, 0, 1],
-        [0, 1, 1, 0]], #beehive
-    2: [[0, 1, 1, 0],
-        [1, 0, 0, 1],
-        [0, 1, 0, 1],
-        [0, 0, 1, 0]], # loaf
-    3: [[1, 1, 0],
-        [1, 0, 1],
-        [0, 1, 0]], # boat
-    4: [[0, 1, 0],
-        [1, 0, 1],
-        [0, 1, 0]], # tub
-    5: [[1],
-        [1],
-        [1]], # blinker
-    6: [[0, 1, 1, 1],
-        [1, 1, 1, 0]], # toad
-    7: [[1, 1, 0, 0],
-        [1, 1, 0, 0],
-        [0, 0, 1, 1],
-        [0, 0, 1, 1]], # beacon
-    8: [[0, 1, 0],
-        [0, 0, 1],
-        [1, 1, 1]], # glider
-    9: [[0, 1, 1],
-        [1, 1, 0],
-        [0, 1, 0]], # r-pentomino
-    10: [[0, 0, 0, 0, 0, 0, 1, 0],
-         [1, 1, 0, 0, 0, 0, 0, 0],
-         [0, 1, 0, 0, 0, 1, 1, 1]], # diehard
-    11: [[0, 1, 0, 0, 0, 0, 0],
-         [0, 0, 0, 1, 0, 0, 0],
-         [1, 1, 0, 0, 1, 1, 1]], # acorn
-    12: [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-         [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]] # gosper glider gun
+    0: [["#", "#"],
+        ["#", "#"]], # block
+    1: [[" ", "#", "#", " "],
+        ["#", " ", " ", "#"],
+        [" ", "#", "#", " "]], #beehive
+    2: [[" ", "#", "#", " "],
+        ["#", " ", " ", "#"],
+        [" ", "#", " ", "#"],
+        [" ", " ", "#", " "]], # loaf
+    3: [["#", "#", " "],
+        ["#", " ", "#"],
+        [" ", "#", " "]], # boat
+    4: [[" ", "#", " "],
+        ["#", " ", "#"],
+        [" ", "#", " "]], # tub
+    5: [["#"],
+        ["#"],
+        ["#"]], # blinker
+    6: [[" ", "#", "#", "#"],
+        ["#", "#", "#", " "]], # toad
+    7: [["#", "#", " ", " "],
+        ["#", "#", " ", " "],
+        [" ", " ", "#", "#"],
+        [" ", " ", "#", "#"]], # beacon
+    8: [[" ", "#", " "],
+        [" ", " ", "#"],
+        ["#", "#", "#"]], # glider
+    9: [[" ", "#", "#"],
+        ["#", "#", " "],
+        [" ", "#", " "]], # r-pentomino
+    10: [[" ", " ", " ", " ", " ", " ", "#", " "],
+         ["#", "#", " ", " ", " ", " ", " ", " "],
+         [" ", "#", " ", " ", " ", "#", "#", "#"]], # diehard
+    11: [[" ", "#", " ", " ", " ", " ", " "],
+         [" ", " ", " ", "#", " ", " ", " "],
+         ["#", "#", " ", " ", "#", "#", "#"]], # acorn
+    12: [[" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#", " ", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#", "#", " ", " ", " ", " ", " ", " ", "#", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#", "#"],
+         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#", " ", " ", " ", "#", " ", " ", " ", " ", "#", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#", "#"],
+         ["#", "#", " ", " ", " ", " ", " ", " ", " ", " ", "#", " ", " ", " ", " ", " ", "#", " ", " ", " ", "#", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+         ["#", "#", " ", " ", " ", " ", " ", " ", " ", " ", "#", " ", " ", " ", "#", " ", "#", "#", " ", " ", " ", " ", "#", " ", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#", " ", " ", " ", " ", " ", "#", " ", " ", " ", " ", " ", " ", " ", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#", " ", " ", " ", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]] # gosper glider gun
 
 }
 
-def initialize(field):
+def ask(question, yesfunc=None, nofunc=None, *args):
+    while True:
+        answer = input(question)
+        answer = answer.lower()
+        print("\n")
+        
+        if answer == "yes" or answer == "y":
+            
+            if yesfunc:
+                return yesfunc(*args)
+                
+            break
+        elif answer == "no" or answer == "n":
+            
+            if nofunc:
+                return nofunc(*args)
+                
+            break
+
+def question(question, lower, upper):
+    answer = input(question)
+    
+    while True:
+        if answer.isdecimal():
+            answer = int(answer)
+            
+            print("\n")
+            if answer >= lower and answer <= upper:
+                return answer
+            
+        answer = input(question)
+        
+def position(pattern, y, x):
+    
+    height = len(pattern)
+    width = len(pattern[0])
+
+    for j in range(height):
+        for k in range(width):
+
+            ypos = y + j
+            xpos = x + k
+
+            if ypos > size[1] - 1:
+                ypos = j
+            if xpos > size[0] - 1:
+                xpos = k
+
+            if j > size[1] - 1:
+                ypos = 0
+            if k > size[0] - 1:
+                xpos = 0
+
+            field[ypos][xpos] = pattern[j][k]
+            duplicate[ypos][xpos] = pattern[j][k]
+
+def horiflip(p):
+    return [row[::-1] for row in p]
+    
+def vertiflip(p):
+    return p[::-1]
+    
+def randinit():
     number = random.randint(4, 12)
 
     for i in range(number):
         n = random.randint(0, len(patterns)-1)
 
-        if i >= number//2:
-            n = random.randint(7, len(patterns)-1)
-
         x = random.randint(0, int(size[0]))
         y = random.randint(0, int(size[1]))
+        
+        orientx = random.randint(0, 1)
+        orienty = random.randint(0, 1)
+        
+        pattern = patterns[n]
 
-        height = len(patterns[n])
-        width = len(patterns[n][0])
+        if orientx:
+            pattern = horiflip(pattern)
 
-        for j in range(height):
-            for k in range(width):
+        if orienty:
+            pattern = vertiflip(pattern)
 
-                ypos = y + j
-                xpos = x + k
+        position(pattern, y, x)
 
-                if ypos > size[1] - 1:
-                    ypos = j
-                if xpos > size[0] - 1:
-                    xpos = k
+def place():
+    for pattern in patterns:
+        patt = [''.join(str(c) for c in lst) for lst in patterns[pattern]]
+        p = '\n'.join(patt)
+        print(pattern + 1, "\n", end = "")
+        print(p, "\n")
+    
+    choice = question("Which pattern would you like to use?\n", 1, 13)
+    
+    selection = patterns[choice-1]
+    selection = ask("Would you like to flip it horizontally?(y/n)\n", horiflip, lambda x: x, selection)
+    selection = ask("Would you like to flip it vertically?(y/n)\n", vertiflip, lambda x: x, selection)
+    
+    print("The pattern:")
+    patt = [''.join(str(c) for c in lst) for lst in selection]
+    p = '\n'.join(patt)
+    print(p)
+    
+    x = question(f"Where would you like to place it on the x-axis, between 0 and {size[0]}?\n", 0, size[0])
+    y = question(f"Where would you like to place it on the y-axis, between 0 and {size[1]}?\n", 0, size[1])
+    
+    position(selection, y, x)
+    
+    cont = ask("Would you like to add another?\n", inputmode, None)
 
-                if j > size[1] - 1:
-                    ypos = 0
-                if k > size[0] - 1:
-                    xpos = 0
+def draw():
+    print("Type the pattern you want. Use # for live cells and [space] for dead cells. Enter for new line.\n")
+    drawing = []
+    while True:
+        row = input()
+        
+        if row:
+            drawing.append(row)
+            
+        else:
+            break
+        
+    drawing = [[ch for ch in row] for row in drawing]
+    
+    maxlen = len(drawing[0])
+    for row in drawing:
+        if len(row) > maxlen:
+            maxlen = len(row)
+    
+    for row in drawing:
+        for i in range(maxlen-len(row)):
+            row.append(" ")
+            
+    print(maxlen)
+    print(drawing)
+    
+    x = question(f"Where would you like to place it on the x-axis, between 0 and {size[0]}?\n", 0, size[0])
+    y = question(f"Where would you like to place it on the y-axis, between 0 and {size[1]}?\n", 0, size[1])
+    
+    position(drawing, y, x)
 
-                field[ypos][xpos] = patterns[n][j][k]
-                duplicate[ypos][xpos] = patterns[n][j][k]
+    cont = ask("Would you like to add another?\n", inputmode, None)
+    
+def inputmode():
+    
+    ask("Would you like to position built-in patterns? [Answer no to draw your own patterns] (y/n)\n", place, draw)
+    
+def conditions():
+    
+    print("Welcome to Conway's Game of Life.")
 
-
-initialize(field)
+    ask("Would you like to input starting conditions?(y/n)\n", inputmode, randinit)
+    
+conditions()
 
 def count(field, y, x):
     neighbors = 0
@@ -108,17 +224,15 @@ def count(field, y, x):
         
     cells = [field[y-1][x-1], field[y-1][x], field[y-1][x1], field[y][x-1], field[y][x1], field[y1][x-1], field[y1][x], field[y1][x1]]
 
-    neighbors = cells.count(1)
+    neighbors = cells.count("#")
 
     return neighbors
 
     
 
 while True:
-    output = [["#" if x == 1 else " " for x in row] for row in field]
 
-
-    outlist = [''.join(str(c) for c in lst) for lst in output]
+    outlist = [''.join(str(c) for c in lst) for lst in field]
     outl = '\n'.join(outlist)
 
     print(outl)
@@ -129,9 +243,9 @@ while True:
             neighbors = count(duplicate, y, x)
             
             if neighbors == 3:
-                field[y][x] = 1
+                field[y][x] = "#"
             elif neighbors < 2 or neighbors > 3:
-                field[y][x] = 0
+                field[y][x] = " "
             
 
     for y in range(len(field)):
